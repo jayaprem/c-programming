89cb211c2ee6e4987a23f90fdf84beee1ae688c5
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-02-20 16:44:48 -0800

    Create Ntimes.c

---
+++ beginner/Ntimes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) 
+{
+	int N;
+	scanf("%d",&N);
+	if(N!=0)
+	{
+	for(int i=1;i<=N;i++)
+	{
+		printf("Hello \n");
+	}
+	}
+	else
+	printf("enter a valid input");
+	return 0;
+}
