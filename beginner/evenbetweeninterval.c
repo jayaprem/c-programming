389e55a61f3eb33b371e23b92102c82d7b8a778d
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-02-25 06:54:57 -0800

    Create evenbetweeninterval.c

---
+++ beginner/evenbetweeninterval.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,k,i;
+	scanf("%d%d",&n,&k);
+	if(n<=100000&&k<=100000)
+	{
+	for(i=n+1;i<k;i++)
+	{
+		if(i%2==0)
+		printf("%d\t",i);
+	}
+	}
+	else
+	printf("invalid");
+	return 0;
+}
