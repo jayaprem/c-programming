e362364f97b4d185784c901398802e41a6d33ef4
jayaprem <36623938+jayaprem@users.noreply.github.com>
2018-08-29 20:36:43 +0530

    Update tree-starpattern.c

--- tree-starpattern.c
+++ tree-starpattern.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-int i,j,k,t,n,s,t1,n1,n2,c;
+int i,j,k,t,n,s,t1,n1,n2,c,a[100];
 scanf("%d %d" ,&s,&n1);
 t1=n1;
 for(t=1;t<=s;t++)
@@ -24,12 +24,14 @@ printf("\n");
 c=c+2;
 n2--;
 }
+a[t]=c;
 n1--;
 }
 printf("\n");
+//printf("%d",a[1]);
 for(i=s;i>0;i--)
 {
-	for(j=1;j<=(c/2);j++)
+	for(j=1;j<a[1]/2;j++)
 	{
 	printf(" ");
 	}
